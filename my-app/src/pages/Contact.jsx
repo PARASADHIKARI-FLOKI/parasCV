d533c707863a0c4d87ab6a018cfb95c3026b7fed
PARASADHIKARI-FLOKI <adparas1@gmail.com>
2025-05-01 11:37:17 +0545

    change

--- my-app/src/pages/Contact.jsx
+++ my-app/src/pages/Contact.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import { FaLinkedin, FaGithub } from 'react-icons/fa';
-import { MdOutlineMailOutline } from 'react-icons/md';
+import React from "react";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { MdOutlineMailOutline } from "react-icons/md";
 
 const Contact = () => {
   return (
-    <div id="contact" className=" bg-[#0a192f] text-white py-16">
+    <div id="contact" className="bg-[#0a192f] text-white py-16">
       <div className="container mx-auto px-4 text-center">
-        <h1 className="text-4xl font-bold mb-6 text-[#64ffda]">Let's Connect</h1>
+        <h1 className="text-4xl font-bold mb-6 text-[#64ffda]">
+          Let's Connect
+        </h1>
         <p className="text-lg max-w-2xl mx-auto mb-10 text-gray-300">
-        As a budding MERN Stack Developer, I’m eager to collaborate, learn, and contribute to meaningful projects. Whether you’re hiring, mentoring, or just want to chat—feel free to reach out!
+          As a budding MERN Stack Developer, I’m eager to collaborate, learn,
+          and contribute to meaningful projects. Whether you’re hiring,
+          mentoring, or just want to chat—feel free to reach out!
         </p>
 
-        <div className="flex justify-center gap-8 text-3xl">
+        <div className="flex justify-center gap-8 text-3xl mb-6">
           <a
-            href="mailto:youremail@example.com"
+            href="https://mail.google.com/mail/?view=cm&to=adhikariparasr@gmail.com"
+            aria-label="Send an email via Gmail"
             className="text-blue-300 hover:text-purple-500 transition-transform transform hover:scale-110"
             target="_blank"
             rel="noopener noreferrer"
@@ -23,6 +28,7 @@ const Contact = () => {
 
           <a
             href="https://www.linkedin.com/in/paras-adhikari-9a5158322/"
+            aria-label="Visit LinkedIn profile"
             className="text-blue-300 hover:text-purple-500 transition-transform transform hover:scale-110"
             target="_blank"
             rel="noopener noreferrer"
@@ -32,6 +38,7 @@ const Contact = () => {
 
           <a
             href="https://github.com/PARASADHIKARI-FLOKI"
+            aria-label="Visit GitHub profile"
             className="text-blue-300 hover:text-purple-500 transition-transform transform hover:scale-110"
             target="_blank"
             rel="noopener noreferrer"
@@ -40,6 +47,7 @@ const Contact = () => {
           </a>
         </div>
       </div>
+      <h1 className="border border-[#203b6a]"></h1>
     </div>
   );
 };
